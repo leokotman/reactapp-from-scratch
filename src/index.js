7e1af50eb1f6b13653348936068e346b556d516a
levkotman <leoktmn@yandex.ru>
2021-04-27 21:48:39 +0200

    task 6
    вопрос по scripts в package.json: в этом изменении я все-таки поставил скрипт, как указано в настройках вебпака и у вас и в методичке: "webpack-dev-server" для сервера. и выдается ошибка. ранее у меня на этот скрипт было написано нечто странное (не помню, откуда взял): webpack serve --open. и все работало. а сейчас даже после npm start нет

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
-const App = ({str}) => {
-  let [value, setValue] = useState(10);
+const App = () => {
+  let [messages, setValue] = useState([]);
 
-  const addPlusToString = () => {
-    return `${str}+${value}`;
-  };
-  const addOneToValue = () => {
-    setValue(value + 1);
+  const addNewMessage = () => {
+    let newMessage = "new message";
+    setValue((messages += newMessage));
   };
 
   return (
     <div>
-      <h1>Hello, React! {addPlusToString()}</h1>
-      <button onClick={addOneToValue}>Update</button>
+      <h1>Hello, React!</h1>
+      <button onClick={addNewMessage}>Add message</button>
+      <span>Your messages:</span>
+      <p> {messages} </p>
     </div>
   );
 };
-ReactDOM.render(<App str="mystring" />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
