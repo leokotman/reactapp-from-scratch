1f69c63e9b72b9d821e2b48c6393486fbeafc3e0
levkotman <leoktmn@yandex.ru>
2021-04-27 20:41:30 +0200

    copied lesson example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  React.createElement("div", null, `Hello React again`),
-  document.getElementById("app")
-);
+const App = ({str}) => {
+  let [value, setValue] = useState(10);
+
+  const addPlusToString = () => {
+    return `${str}+${value}`;
+  };
+  const addOneToValue = () => {
+    setValue(value + 1);
+  };
+
+  return (
+    <div>
+      <h1>Hello, React! {addPlusToString()}</h1>
+      <button onClick={addOneToValue}>Update</button>
+    </div>
+  );
+};
+ReactDOM.render(<App str="mystring" />, document.getElementById("app"));
