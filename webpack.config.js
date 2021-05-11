1238934c5242332321ddf950ffd8b4573740b635
levkotman <leoktmn@yandex.ru>
2021-05-11 02:09:01 +0200

    added html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -48,6 +49,12 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
   devServer: {
     contentBase: path.resolve(__dirname, "build"),
     open: true,
